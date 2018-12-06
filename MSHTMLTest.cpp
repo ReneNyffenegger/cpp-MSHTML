fb775c4cd5e5cb605604b0865f3670765a4f3d85
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2018-12-06 13:23:46 +0100

    add img etc.

--- MSHTMLTest.cpp
+++ MSHTMLTest.cpp
@@ -1,27 +1,40 @@
 #include <windows.h>
+#include <string>
+
 #include "HTMLWindow.h"
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE /*unused__*/, LPSTR /*lpszCmdLine*/, int /*nCmdShow*/) {
 
-  HTMLWindow* html_window = new HTMLWindow (
+  char curDir[256];
+  GetCurrentDirectoryA(255, curDir);
 
-      // Parameter html_or_url:
-      "<html><head>"
-      "<title>MSHTMLTest</title>"   // seems a little useless in this context
-      "</head><body>"
-      "<h1>This is a test</h1>"
-      "I offer the following links:"
-      "<ul>"
-      "<li><a href='http://www.google.com'>www.google.com</a>"
-      "<li><a href='http://www.adp-gmbh.ch'>www.adp-gmbh.ch</a>"
-      "<li><a href='http://www.yahoo.com'>www.yahoo.com</a>"
-      "</ul>"
-      "</body></html>",
-      // Parameter title 
-       "MSHTMLTest", 
-       hInstance,
-      false  // indicates: this not an url, but html 
-       );
+  std::string html = 
+           "<html><head>"
+           "<title>MSHTMLTest</title>"   // seems a little useless in this context
+           "</head><body>"
+           "<h1>This is a test</h1>"
+           "I offer the following links:"
+           "<ul>"
+           "<li><a href='https://www.google.com'>Google</a>"
+           "<li><a href='https://www.renenyffenegger.ch'>renenyffenegger.ch</a>"
+           "<li><a href='https://github.com/ReneNyffenegger/cpp-MSHTML'>github repository</a>"
+       //
+       //  Unfortunately, file:// links don't seem to work:
+       //
+       //  "<li><a href='file://"; html += curDir;  html += "/test/index.html'>test/index.html</a>"
+           ;
+  html += "</ul>"
+          "</body></html>",
+        // Parameter title 
+          "MSHTMLTest";
+//MessageBox(0, html.c_str(), 0, 0);
+
+  HTMLWindow* html_window = new HTMLWindow (
+         html,         // HTML text (or url)
+        "MSHTML Test", // Title
+         hInstance,
+         false  // indicates: this not an url, but html 
+  );
 
   MSG msg;
   while (GetMessage(&msg, 0, 0, 0)) {
